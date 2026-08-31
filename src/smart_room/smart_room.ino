6b9c8ec61fdaa05b242414d5d39ad18e9c2d26f0
Leart-Kaceli <leart.kaceli@gmail.com>
2026-08-31 16:48:47 -0400

    v0.1 - Add temperature monitoring

--- src/smart_room/smart_room.ino
+++ src/smart_room/smart_room.ino
@@ -1,1 +1,19 @@
+const int tempSensorPin = A0;
 
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  int tempSensorValue = analogRead(tempSensorPin);
+
+  float voltage = (tempSensorValue/1024.0) * 5.0;
+  float tempC = (voltage - .5) * 100;
+  Serial.println("Temperature in Celcius: ");
+  Serial.println(tempC);
+  float tempF = (tempC * 9.0/5.0) + 32.0;
+  Serial.println("Temperature in Farenheit ");
+  Serial.println(tempF);
+
+  delay(500);
+}
