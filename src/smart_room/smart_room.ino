cf8a7e2479f0fbd6887e1f6b988da52ad819b63b
Leart-Kaceli <leart.kaceli@gmail.com>
2026-09-01 18:38:21 -0400

    v0.2 - Add light monitoring

--- src/smart_room/smart_room.ino
+++ src/smart_room/smart_room.ino
@@ -1,19 +1,26 @@
 const int tempSensorPin = A0;
+const int lightSensorPin = A1;
 
-void setup(){
+void setup() {
   Serial.begin(9600);
 }
 
-void loop(){
+void loop() {
+  int lightSensorValue = analogRead(lightSensorPin);
+  analogRead(tempSensorPin);
+  delay(20);
   int tempSensorValue = analogRead(tempSensorPin);
 
-  float voltage = (tempSensorValue/1024.0) * 5.0;
-  float tempC = (voltage - .5) * 100;
-  Serial.println("Temperature in Celcius: ");
-  Serial.println(tempC);
-  float tempF = (tempC * 9.0/5.0) + 32.0;
-  Serial.println("Temperature in Farenheit ");
+  float voltage = (tempSensorValue / 1024.0) * 5.0;
+  float rawTempC = (voltage - 0.5) * 100;
+  float rawTempF = (rawTempC * 9.0 / 5.0) + 32.0;
+  float tempF = rawTempF - 10.0;
+
+  Serial.print("Temperature in Fahrenheit: ");
   Serial.println(tempF);
+
+  Serial.print("Light Level: ");
+  Serial.println(lightSensorValue);
 
   delay(500);
 }
